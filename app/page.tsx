62437871eb328a814b8c45d4baaca345feb90bf3
KonstantinHerasimenko <konstantin.herasimenko@gmail.com>
2026-01-27 20:39:53 +0300

    Complete loading page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import StarIcon from './icons/star.svg';
+import StarIcon from './icons/star.svg'
 
 export default function Home() {
-  return (
-    <main>
-      <StarIcon 
-        className="w-24 h-24" 
-        title="Star icon"
-      />
-    </main>
-  );
+	return <main>Hello world</main>
 }
